b03c9e292677b5d9abfc2b741fcbf4f9e5a14ecb
jhk828 <37868396+jhk828@users.noreply.github.com>
2020-11-17 10:51:11 +0900

    Update search.py

--- search.py
+++ search.py
@@ -4,7 +4,7 @@ def search_map(search_title):
     searching = search_title
     url = 'https://dapi.kakao.com/v2/local/search/keyword.json?query={}'.format(searching)
     headers = {
-        "Authorization": "KakaoAK 2e82a25b8891cd07592a331983cfc534"
+        "Authorization": "KakaoAK 카카오Key"
     }
     places = requests.get(url, headers = headers).json()['documents']
     try:
